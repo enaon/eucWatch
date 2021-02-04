a160183c5fa1cf8f2c179e9654d5b395ced39ca5
enaon <enaon@enaon>
2021-02-04 13:59:25 +0200

    Update euc_dash.js

--- P8/euc_simple/euc_dash.js
+++ P8/euc_simple/euc_dash.js
@@ -167,7 +167,7 @@ face[0] = {
   	  this.g.setColor(0,col("black"));
 	  this.g.fillRect(0,56,239,239);
       this.g.setColor(1,col("white"));
-      this.g.setFont("Vector",60);
+      this.g.setFont("Vector",50);
       this.g.drawString(euc.conn,(125-this.g.stringWidth(euc.conn)/2),115);
       this.g.flip();
 	  if (euc.conn=="WAIT"){this.spd[0]=-1;this.spd[1]=-1;this.amp=-1;this.temp=-1;this.batt=-1;this.trpN=-1;this.conn="OFF";this.lock=2;this.run=true;}
