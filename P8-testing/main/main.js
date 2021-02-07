11d026d230086164f20a326bb9a146e21b5f79e2
enaon <enaon@enaon>
2021-02-07 02:19:00 +0200

    Update main.js

--- P8-testing/main/main.js
+++ P8-testing/main/main.js
@@ -313,7 +313,7 @@ touchHandler[0]=function(e,x,y){
 		if (Boolean(require("Storage").read("settings"))) {face.go("settings",0);return;}
 	  } else digitalPulse(D16,1,40);
     }else if  (e==3){
-		if (Boolean(require("Storage").read("dash"))) {face.go("dash",0);return;}
+		if (Boolean(require("Storage").read(set.dash[set.def.dash]))) {face.go(set.dash[set.def.dash],0);return;}
 		else if (Boolean(require("Storage").read("alarm"))) {face.go("alarm",0);return;}
     }else if  (e==4){
 		if (Boolean(require("Storage").read("notify"))) {face.go("notify",0);return;}
