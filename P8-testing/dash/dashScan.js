eb6ee5eee2d58b730e3e099b7afd568dfb6335c7
enaon <enaon@enaon>
2021-02-10 07:49:05 +0200

    Update dashScan.js

--- P8-testing/dash/dashScan.js
+++ P8-testing/dash/dashScan.js
@@ -75,7 +75,7 @@ touchHandler[0]=function(e,x,y){
 	//ninebot
     }else if(100<y&&y<200) {
 	  digitalPulse(D16,1,[30,50,30]);
-	  (s=>{s&&(s["slot"+require("Storage").readJSON("dash.json",1).slot+"_maker"]="ninebot")&&require('Storage').write('dash.json',s);})(require('Storage').readJSON('dash.json',1));
+	  (s=>{s&&(s["slot"+require("Storage").readJSON("dash.json",1).slot+"_maker"]="ninebotOE")&&require('Storage').write('dash.json',s);})(require('Storage').readJSON('dash.json',1));
       face.appCurr=set.dash[set.def.dash];
       face.go('w_scan',0,'ffe0');
     }else digitalPulse(D16,1,40); 
