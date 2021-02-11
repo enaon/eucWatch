2d641a7b3c987f6ca07e52f96c74bf32612b31c6
enaon <enaon@enaon>
2021-02-11 11:41:46 +0200

    Update dashSlot.js

--- P8-testing/dash/dashSlot.js
+++ P8-testing/dash/dashSlot.js
@@ -4,20 +4,54 @@ face[0] = {
 	g:w.gfx, 
 	init: function(o){ 
 		this.g.setColor(0,col("black"));
-		this.g.setColor(1,col("dgray"));
+        this.g.setColor(1,col("dgray"));
+        this.g.fillRect(0,0,75,75);//1
+        this.g.fillRect(80,0,155,75); //2
+        this.g.fillRect(160,0,239,75); //3
+        this.g.fillRect(0,80,75,155); //4
+        this.g.fillRect(80,80,155,155); //5
+        this.g.fillRect(160,80,239,155);//6
+        this.g.flip();
+      
+		this.g.setColor(0,col("dgray"));
 		this.g.fillRect(0,0,75,75);//1
+		this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("SPEED",40-(this.g.stringWidth("SPEED")/2),30); 
+		this.g.flip();	
+   		this.g.setColor(0,col("dgray"));
 		this.g.fillRect(80,0,155,75); //2
+      	this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("AMP",120-(this.g.stringWidth("AMP")/2),30); 
+      	this.g.flip();	
+		this.g.setColor(0,col("dgray"));
 		this.g.fillRect(160,0,239,75); //3
+        this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("TEMP",200-(this.g.stringWidth("TEMP")/2),30); 
+      	this.g.flip();	
+		this.g.setColor(0,col("dgray"));
 		this.g.fillRect(0,80,75,155); //4
-		this.g.fillRect(80,80,155,155); //5
+        this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("TEMP",40-(this.g.stringWidth("TEMP")/2),110); 
+      	this.g.flip();      
+		this.g.setColor(0,col("dgray"));
+        this.g.fillRect(80,80,155,155); //5
+        this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("TEMP",120-(this.g.stringWidth("TEMP")/2),110); 
+      	this.g.flip();
+		this.g.setColor(0,col("dgray"));
 		this.g.fillRect(160,80,239,155);//6
-		this.g.flip();
+        this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("TEMP",200-(this.g.stringWidth("TEMP")/2),110); 
+      	this.g.flip();      
 	},
 	show : function(o){
-	    this.g.setColor(1,col("white"));
-		this.g.setFont("Vector",16);
-		this.g.drawString("SPEED",60-(this.g.stringWidth("SPEED")/2),38); 
-		this.g.flip();	
+
 	},
 	tid:-1,
 	run:false,
@@ -36,7 +70,8 @@ face[1] = {
 	offms:1000,
 	init: function(){return true;},
 	show : function(){
-		face.go(face.appRoot[0],face.appRoot[1]); //go to the previous face on screen of the previous app.  
+        face.go("dashSelect",0);
+		//face.go(face.appRoot[0],face.appRoot[1]); //go to the previous face on screen of the previous app.  
 		//face.go(face.appPrev,face.pagePrev); //go to the previous face on screen, even if it was on the same app. 
 		//face.go("hello",-1); //sleep and set this face as the on_wake face. 
 		//face.go("main",-1);//sleep and set this face as the on_wake face. 
