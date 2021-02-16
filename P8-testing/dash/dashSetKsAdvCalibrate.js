5540f399d2c7e8fc714f9ab7f6030f8605fa2f25
enaon <enaon@enaon>
2021-02-16 16:58:14 +0200

    .

--- P8-testing/dash/dashSetKsAdvCalibrate.js
+++ P8-testing/dash/dashSetKsAdvCalibrate.js
@@ -1,64 +1,69 @@
-//kingsong  set options
+//kingsong  set adv calibrate
 face[0] = {
 	offms: 5000,
 	g:w.gfx,
 	init: function(){
-		this.g.setColor(0,col("black"));
-		this.g.fillRect(0,198,239,239);
+		this.g.setColor(0,col("dgray"));
+		this.g.fillRect(0,0,239,177);
 		this.g.setColor(1,col("white"));
 		this.g.setFont("Vector",20);
-		this.g.drawString("CALIBRATION",120-(this.g.stringWidth("CALIBRATION")/2),214); 
+		this.g.drawString("SET PEDAL TILT",120-(this.g.stringWidth("SET PEDAL TILT")/2),10); 		
+//		this.g.drawString("MANUALY SET PEDAL TILT",120-(this.g.stringWidth("MANUALY SET PEDAL TILT")/2),5);
+		this.g.drawImage(require("heatshrink").decompress(atob("oFAwJC/AAs8A41+A43/AwsDA40HA40PA40f/wHFn/8Fw34AwkB//wGw3AGw2AGxk/Gw1/Gw4uFGwPgGxguBGwsfGw4uGv5lFGw4HBGwoHJC4wnHG45HHK45nHO444JGAynHW47HHHBKBHNJ44QA4o4BA4owBA41+A408A4wA6A==")),0,70);
+		this.g.drawImage(require("heatshrink").decompress(atob("oFAwJC/AAU8A41+A43/A4/AA43gA43wA4t//AHFn/8A4sfGA0P/+AA4kDHA0BHCAwGn/+GA4HFg44QGA3/NJ44QA5oXHE443HI4xXHM453HGw6XHU44uGY442Hc473HMo9/Voy9Ifw42FA4IGFgF+A408A4wA9A=")),180,70);
 		this.g.flip(); 
-		this.g.setColor(0,col("dgray"));
-		this.g.fillRect(0,0,239,150);
+		this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",80);
+		this.g.drawString(euc.dash.tiltSet,130-(this.g.stringWidth(euc.dash.tiltSet)/2),65); 		
+		this.g.flip(); 
+        this.g.setColor(0,col("blue1"));
+		this.g.fillRect(0,177,239,239);
 		this.g.setColor(1,col("white"));
 		this.g.setFont("Vector",20);
-		this.g.drawString("MANUALY SET PEDAL TILT",120-(this.g.stringWidth("MANUALY SET PEDAL TILT")/2),5); 		
-		this.g.drawString("MANUALY SET PEDAL TILT",120-(this.g.stringWidth("MANUALY SET PEDAL TILT")/2),5);
-		this.g.drawImage(require("heatshrink").decompress(atob("oFAwJC/AAs8A41+A43/AwsDA40HA40PA40f/wHFn/8Fw34AwkB//wGw3AGw2AGxk/Gw1/Gw4uFGwPgGxguBGwsfGw4uGv5lFGw4HBGwoHJC4wnHG45HHK45nHO444JGAynHW47HHHBKBHNJ44QA4o4BA4owBA41+A408A4wA6A==")),210,100);
-		this.g.drawImage(require("heatshrink").decompress(atob("oFAwJC/AAU8A41+A43/A4/AA43gA43wA4t//AHFn/8A4sfGA0P/+AA4kDHA0BHCAwGn/+GA4HFg44QGA3/NJ44QA5oXHE443HI4xXHM453HGw6XHU44uGY442Hc473HMo9/Voy9Ifw42FA4IGFgF+A408A4wA9A=")),20,100);
+   		this.g.drawString("START",120-(this.g.stringWidth("START")/2),188); 
+		this.g.drawString("CALIBRATION",120-(this.g.stringWidth("CALIBRATION")/2),216); 
 		this.g.flip(); 
-		this.g.setColor(0,col("dgray"));
-		this.g.fillRect(50,50,190,150);
-		this.g.setColor(1,col("white"));
-		this.g.setFont("Vector",60);
-		this.g.drawString(euc.dash.tiltSet,120-(this.g.stringWidth(euc.dash.tiltSet)/2),90); 		
-
-//prev=require("heatshrink").decompress(atob("oFAwJC/AAs8A41+A43/AwsDA40HA40PA40f/wHFn/8Fw34AwkB//wGw3AGw2AGxk/Gw1/Gw4uFGwPgGxguBGwsfGw4uGv5lFGw4HBGwoHJC4wnHG45HHK45nHO444JGAynHW47HHHBKBHNJ44QA4o4BA4owBA41+A408A4wA6A=="))		
-//next=require("heatshrink").decompress(atob("oFAwJC/AAU8A41+A43/A4/AA43gA43wA4t//AHFn/8A4sfGA0P/+AA4kDHA0BHCAwGn/+GA4HFg44QGA3/NJ44QA5oXHE443HI4xXHM453HGw6XHU44uGY442Hc473HMo9/Voy9Ifw42FA4IGFgF+A408A4wA9A="))
-		this.b1=-1;
-		this.b2=-1;
-		this.b3=-1;
-		this.b4=-1;
-		this.info=0;
-		this.firstrun=1;
+		this.calibrate=0;
 		this.run=true;
 	},
 	show : function(){
 		if (euc.state!=="READY") {face.go(set.dash[set.def.dash],0);return;}
 		if (!this.run) return; 
-		if (this.b1!=euc.dash.aLck){ //auto lock
-			this.b1=euc.dash.aLck;
-			this.g.setColor(0,(this.b1)?col("raf"):col("dgray"));
-			this.g.fillRect(0,0,118,97);
-			this.g.setColor(1,col("white"));
-			this.g.setFont("Vector",18);	
-			this.g.drawString("AUTO",60-(this.g.stringWidth("AUTO")/2),15); 
-			this.g.setFont("Vector",28);	
-			this.g.drawString("LOCK",60-(this.g.stringWidth("LOCK")/2),50); 
-			this.g.flip();
-            if (!this.firstrun) {
-			this.g.setColor(0,col("raf3"));
-			this.g.fillRect(0,198,239,239);//6
-			this.g.setColor(1,col("white"));
-			this.g.setFont("Vector",16);
-			 this.g.drawString((this.b1)?"DISCONNECT -> LOCK":"AUTO LOCK DISABLED",120-(this.g.stringWidth((this.b1)?"DISCONNECT -> LOCK":"AUTO LOCK DISABLED")/2),214); 
-			this.info=1;
-			this.g.flip();
-			}
+        if (this.calibrate) {
+            this.g.setColor(0,col("dgray"));
+		    this.g.fillRect(0,0,239,174);                    
+            this.g.setColor(1,col("white"));
+		    this.g.setFont("Vector",17);
+		    this.g.drawString("1. PRESS START",25,10); 	
+   		    this.g.drawString("2. LEVEL WHEEL",25,37); 		
+            this.g.drawString("3. TURN WHEEL ON",25,64);
+            this.g.drawString("4. WHEEL BEEPS->OFF",25,91); 
+            this.g.drawString("5. TURN WHEEL ON",25,118);
+            this.g.drawString("5. DONE!",25,145);
+		    this.g.flip();
+            this.g.setColor(0,col("raf"));
+		    this.g.fillRect(0,175,120,239);                    
+            this.g.setColor(1,col("white"));
+   		    this.g.setFont("Vector",22);
+            this.g.drawString("START",15,200); 	
+		    this.g.flip();
+            this.g.setColor(0,col("dgray"));
+		    this.g.fillRect(121,175,239,239);                    
+            this.g.setColor(1,col("white"));
+   		    this.g.setFont("Vector",22);
+            this.g.drawString("CANCEL",140,200); 	
+		    this.g.flip();
+       		this.run=false;
+        }else if (this.tilt!=euc.dash.tiltSet){ //tilt Set
+		    this.g.setColor(0,col("dgray"));
+		    this.g.fillRect(50,50,190,150);                    
+            this.g.setColor(1,col("white"));
+		    this.g.setFont("Vector",80);
+		    this.g.drawString(euc.dash.tiltSet,130-(this.g.stringWidth(euc.dash.tiltSet)/2),65); 		
+		    this.g.flip();
 		}
-		
-		if (this.info)  {
+
+        if (this.info)  {
 			this.info=0;
 			if (this.itid)clearTimeout(this.itid);
 			this.itid=setTimeout(function(t){
@@ -109,19 +114,22 @@ face[1] = {
 touchHandler[0]=function(e,x,y){ 
 	switch (e) {
 	case 5: //tap event
-		if (x<=120&&y<100) { //auto lock
-			euc.dash.aLck=1-euc.dash.aLck;
+        if (!face[0].calibrate){
+		if (x<=120&&y<175) { //tilt forward
+			euc.dash.tiltSet--;euc.wri("tiltSet");
 			digitalPulse(D16,1,[30,50,30]);
-		}else if (120<=x<=239&&y<=100) { //buzz
-			euc.dash.buzz=1-euc.dash.buzz;
+		}else if (120<=x&&y<=175) { //tilt back
+			euc.dash.tiltSet++;euc.wri("tiltSet");
 			digitalPulse(D16,1,[30,50,30]);
-		}else if (x<=120&&100<=y<=200) { //auto off
-			euc.dash.aOff=1-euc.dash.aOff;
-			digitalPulse(D16,1,[30,50,30]);		
-		}else if (120<=x<=239&&100<=y<=200) { //lock
-			euc.dash.horn=1-euc.dash.horn;
-			digitalPulse(D16,1,[30,50,30]);						
+		}else if (175<=y) { //calibrate
+            face[0].calibrate=1;
+			digitalPulse(D16,1,[30,50,30]);
 		}else digitalPulse(D16,1,[30,50,30]);
+        }else { //calibrate
+          if (175<=y&&120<=x) {face[0].init();face[0].show();
+          }else if (175<=y&&x<=120) euc.wri("calibrate");
+          else digitalPulse(D16,1,40);
+        }
 		this.timeout();
 		break;
 	case 1: //slide down event
@@ -139,7 +147,7 @@ touchHandler[0]=function(e,x,y){
 		this.timeout();
 		break;
 	case 3: //slide left event
-		digitalPulse(D16,1,40
+		digitalPulse(D16,1,40);
 		this.timeout();
 		break;
 	case 4: //slide right event (back action)
