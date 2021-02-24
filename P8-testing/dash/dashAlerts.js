955bb692b3f9d1a12ba5637b13a2a037380f16f0
enaon <enaon@enaon>
2021-02-24 06:17:27 +0200

    update dash

--- P8-testing/dash/dashAlerts.js
+++ P8-testing/dash/dashAlerts.js
@@ -11,10 +11,14 @@ face[0] = {
 		this.g.setFont("Vector",20);
 		this.g.drawString("HAPTIC ALERTS",120-(this.g.stringWidth("HAPTIC ALERTS")/2),217); 
 		this.g.flip(); 
-		this.btn(euc.dash.hapS,"SPEED",24,60,20,col("blue1"),col("raf4"),0,0,119,97,(euc.dash.hapS)?"HAPTIC ON":"HAPTIC OFF",14,60,70);
-		this.btn(euc.dash.hapA,"AMPERE",24,185,20,col("blue1"),col("raf4"),122,0,239,97,(euc.dash.hapA)?"HAPTIC ON":"HAPTIC OFF",14,185,70);
-		this.btn(euc.dash.hapT,"TEMP",24,60,120,col("blue1"),col("raf4"),0,100,119,195,(euc.dash.hapT)?"HAPTIC ON":"HAPTIC OFF",14,60,170);
-        this.btn(euc.dash.hapB,"BATT",24,185,120,col("blue1"),col("raf4"),122,100,239,195,(euc.dash.hapB)?"HAPTIC ON":"HAPTIC OFF",14,185,170);		
+//		this.btn(euc.dash.hapS,"SPEED",24,60,20,col("blue1"),col("raf4"),0,0,119,97,(euc.dash.hapS)?"HAPTIC ON":"HAPTIC OFF",14,60,70);
+//		this.btn(euc.dash.hapA,"AMP",24,185,20,col("blue1"),col("raf4"),122,0,239,97,(euc.dash.hapA)?"HAPTIC ON":"HAPTIC OFF",14,185,70);
+//		this.btn(euc.dash.hapT,"TEMP",24,60,120,col("blue1"),col("raf4"),0,100,119,195,(euc.dash.hapT)?"HAPTIC ON":"HAPTIC OFF",14,60,170);
+//        this.btn(euc.dash.hapB,"BATT",24,185,120,col("blue1"),col("raf4"),122,100,239,195,(euc.dash.hapB)?"HAPTIC ON":"HAPTIC OFF",14,185,170);		
+		this.btn(euc.dash.hapS,"SPEED",25,60,37,col("blue1"),col("raf4"),0,0,119,97);
+		this.btn(euc.dash.hapA,"AMP",25,185,37,col("blue1"),col("raf4"),122,0,239,97);
+		this.btn(euc.dash.hapT,"TEMP",25,60,136,col("blue1"),col("raf4"),0,100,119,195);
+        this.btn(euc.dash.hapB,"BATT",25,185,136,col("blue1"),col("raf4"),122,100,239,195);			
 		 //clear
 		this.g.setColor(0,0);
 		this.g.drawLine (0,98,239,98);
@@ -104,45 +108,45 @@ touchHandler[0]=function(e,x,y){
 					if (euc.dash.haSv=="spd1") euc.dash.haSv="spd2";
 					else if (euc.dash.haSv=="spd2") euc.dash.haSv="spd3";
 					else euc.dash.haSv="spd1";
-					face[0].btn(1,"<      SPEED      >",20,120,20,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv]+" Km/h",25,120,60);
+                    face[0].btn(1,"SPEED (IN Km/h)",18,120,8,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv],50,120,40);
 					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
 				}else{ //step
 					if (x<=120){ if (1<euc.dash.spdS) euc.dash.spdS--;
 					}else if (euc.dash.spdS<5) euc.dash.spdS++;
-					face[0].btn(1,"<       STEP       >",20,120,120,col("dgray"),0,0,100,239,195,euc.dash.spdS+ " km/h",25,120,160);
+			        face[0].btn(1,"STEP (IN Km/h)",18,120,110,col("gray"),0,0,100,239,195,euc.dash.spdS,50,120,140);
 					face[0].ntfy("ONE PULSE PER:","",18,col("dgray"),1);
 				}  
 			}else if (face[0].set=="amp") { //amp
 				if (y<=65){ //uphill
 					if (120<=x&&euc.dash.ampH<35) euc.dash.ampH++;
 					else if (x<=120&&15<euc.dash.ampH) euc.dash.ampH--;
-					face[0].btn(1," UPHILL:",20,80,20,col("dgray"),0,0,0,239,63,euc.dash.ampH+" A",28,200,20);
+                    face[0].btn(1,"UPHILL:",20,60,23,col("gray"),0,0,0,239,63,euc.dash.ampH+" A",35,180,16);
 					face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);
 				}else if (65<=y&&y<=133){//braking
 					if (x<=120&&euc.dash.ampL<-5) euc.dash.ampL++;
 					else if (120<=x&&-15<euc.dash.ampL) euc.dash.ampL--;
-					face[0].btn(1,"BRAKING:",20,80,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",28,200,90);
+			        face[0].btn(1,"BRAKING:",20,60,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",35,180,84);
 					face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);                    
 				}else {//step
 					if (120<=x&&euc.dash.ampS<3) euc.dash.ampS++;
 					else if (x<=120&&1<euc.dash.ampS) euc.dash.ampS--;
-					face[0].btn(1,"STEP:",20,80,160,col("dgray"),0,0,135,239,195,euc.dash.ampS+ " A",28,200,160);
+			        face[0].btn(1,"STEP:",20,60,157,col("gray"),0,0,135,239,195,euc.dash.ampS+ " A",35,180,150);
 					face[0].ntfy("ONE PULSE PER:","",18,col("dgray"),1);
 				}
             }else if (face[0].set=="temp") { //temp
               if (y<=120){ //
 					if (120<=x&&euc.dash.tmpH<70) euc.dash.tmpH++;
    			  		else if (x<=120&&50<euc.dash.tmpH) euc.dash.tmpH--;
-                    face[0].btn(1,"SET TEMPERATURE:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,50,120,45);
+                    face[0].btn(1,"SET HI-TEMP:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,50,120,41);
 					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
 				}else{ //back
                   face[0].set=0;face[0].init();
                 }
-            }else if (face[0].set=="bat") { //temp
+            }else if (face[0].set=="batt") { //temp
               if (y<=120){ //
 					if (120<=x&&euc.dash.batL<30) euc.dash.batL++;
    			  		else if (x<=120&&5<euc.dash.batL) euc.dash.batL--;
-                    face[0].btn(1,"SET BATTERY:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,50,120,45);
+                    face[0].btn(1,"SET LOW-BATT:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,50,120,41);
 					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
 				}else{ //back
                   face[0].set=0;face[0].init();
@@ -152,22 +156,22 @@ touchHandler[0]=function(e,x,y){
         }else{
 			if (x<=120&&y<100) { //Speed
 				euc.dash.hapS=1-euc.dash.hapS;
-				face[0].btn(euc.dash.hapS,"SPEED",24,60,20,col("blue1"),col("raf4"),0,0,119,97,(euc.dash.hapS)?"HAPTIC ON":"HAPTIC OFF",14,60,70);
+				face[0].btn(euc.dash.hapS,"SPEED",25,60,37,col("blue1"),col("raf4"),0,0,119,97);
 				face[0].ntfy("HOLD -> SET SPEED","HOLD -> SET SPEED",18,col("dgray"),1);
 				digitalPulse(D16,1,[30,50,30]);
 			}else if (120<=x&&y<=100) { //Ampere
 				euc.dash.hapA=1-euc.dash.hapA;
-				face[0].btn(euc.dash.hapA,"AMPERE",24,185,20,col("blue1"),col("raf4"),122,0,239,97,(euc.dash.hapA)?"HAPTIC ON":"HAPTIC OFF",14,185,70);
+				face[0].btn(euc.dash.hapA,"AMP",25,185,37,col("blue1"),col("raf4"),122,0,239,97);
 				face[0].ntfy("HOLD -> SET AMPERE","",18,col("dgray"),1);
 				digitalPulse(D16,1,[30,50,30]);
 			}else if (x<=120&&100<=y) { //Temp
 				euc.dash.hapT=1-euc.dash.hapT;
-				face[0].btn(euc.dash.hapT,"TEMP",24,60,120,col("blue1"),col("raf4"),0,100,119,195,(euc.dash.hapT)?"HAPTIC ON":"HAPTIC OFF",14,60,170);
+				face[0].btn(euc.dash.hapT,"TEMP",25,60,136,col("blue1"),col("raf4"),0,100,119,195);
 				face[0].ntfy("HOLD -> SET TEMP","",18,col("dgray"),1);
 				digitalPulse(D16,1,[30,50,30]);		
 			}else if (120<=x&&100<=y) { //Batt
 				euc.dash.hapB=1-euc.dash.hapB;
-				face[0].btn(euc.dash.hapB,"BATT",24,185,120,col("blue1"),col("raf4"),122,100,239,195,(euc.dash.hapB)?"HAPTIC ON":"HAPTIC OFF",14,185,170);
+				face[0].btn(euc.dash.hapB,"BATT",25,185,136,col("blue1"),col("raf4"),122,100,239,195);
 				face[0].ntfy("HOLD -> SET BATTERY","",18,col("dgray"),1);
 				digitalPulse(D16,1,[30,50,30]);						
 			}else digitalPulse(D16,1,[30,50,30]);
@@ -206,25 +210,25 @@ touchHandler[0]=function(e,x,y){
         }else if (x<=120&&y<100) { //spd
 			face[0].set="spd";
             digitalPulse(D16,1,[30,50,30]);
-            face[0].btn(1,"<      SPEED      >",20,120,20,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv]+" Km/h",25,120,60);
-			face[0].btn(1,"<       STEP       >",20,120,120,col("dgray"),0,0,100,239,195,euc.dash.spdS+ " km/h",25,120,160);
+            face[0].btn(1,"SPEED (IN Km/h)",18,120,8,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv],50,120,40);
+			face[0].btn(1,"STEP (IN Km/h)",18,120,110,col("gray"),0,0,100,239,195,euc.dash.spdS,50,120,140);
 		}else if (120<=x&&y<=100) { //amp
 			face[0].set="amp";
 			digitalPulse(D16,1,[30,50,30]);
             w.gfx.setColor(0,0);
 	    	w.gfx.fillRect(0,0,239,195);
     		w.gfx.flip();
-            face[0].btn(1," UPHILL:",20,80,20,col("dgray"),0,0,0,239,63,euc.dash.ampH+" A",28,200,20);
-			face[0].btn(1,"BRAKING:",20,80,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",28,200,90);
-			face[0].btn(1,"STEP:",20,80,160,col("dgray"),0,0,135,239,195,euc.dash.ampS+ " A",28,200,160);
+            face[0].btn(1,"UPHILL:",20,60,23,col("gray"),0,0,0,239,63,euc.dash.ampH+" A",35,180,16);
+			face[0].btn(1,"BRAKING:",20,60,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",35,180,84);
+			face[0].btn(1,"STEP:",20,60,157,col("gray"),0,0,135,239,195,euc.dash.ampS+ " A",35,180,150);
 		}else if (x<=120&&100<=y) { //temp
 			face[0].set="temp";
             digitalPulse(D16,1,[30,50,30]);
-            face[0].btn(1,"SET TEMPERATURE:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,50,120,45);
-		}else if (120<=x&&100<=y) { //bat
-			face[0].set="bat";
+            face[0].btn(1,"SET HI-TEMP:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,50,120,40);
+		}else if (120<=x&&100<=y) { //batt
+			face[0].set="batt";
             digitalPulse(D16,1,[30,50,30]);
-            face[0].btn(1,"SET BATTERY:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,50,120,45);
+            face[0].btn(1,"SET LOW-BATT:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,50,120,40);
 		}else digitalPulse(D16,1,[30,50,30]);		
 		this.timeout();
 		break;
--- P8-testing/dash/dashSetKsOpt.js
+++ P8-testing/dash/dashSetKsOpt.js
@@ -1,28 +1,28 @@
-//kingsong  Alerts
+//kingsong  set options
 face[0] = {
 	offms: 5000,
 	g:w.gfx,
 	init: function(){
 		if (euc.state!=="READY") {face.go(set.dash[set.def.dash],0);return;}
-        if (!face.appPrev.startsWith("dashSet")) this.g.clear();
+        if (!face.appPrev.startsWith("dash")) this.g.clear();
         this.g.setColor(0,0);
-		this.g.fillRect(0,196,239,239);
+		this.g.fillRect(0,205,239,239);
 		this.g.setColor(1,col("white"));
 		this.g.setFont("Vector",20);
-		this.g.drawString("HAPTIC ALERTS",120-(this.g.stringWidth("HAPTIC ALERTS")/2),217); 
+		this.g.drawString("OPTIONS",120-(this.g.stringWidth("OPTIONS")/2),217); 
 		this.g.flip(); 
-		this.btn(euc.dash.hapS,"SPEED",24,60,20,col("blue1"),col("raf4"),0,0,119,97,(euc.dash.hapS)?"HAPTIC ON":"HAPTIC OFF",14,60,70);
-		this.btn(euc.dash.hapA,"AMPERE",24,185,20,col("blue1"),col("raf4"),122,0,239,97,(euc.dash.hapA)?"HAPTIC ON":"HAPTIC OFF",14,185,70);
-		this.btn(euc.dash.hapT,"TEMP",24,60,120,col("blue1"),col("raf4"),0,100,119,195,(euc.dash.hapT)?"HAPTIC ON":"HAPTIC OFF",14,60,170);
-        this.btn(euc.dash.hapB,"BATT",24,185,120,col("blue1"),col("raf4"),122,100,239,195,(euc.dash.hapB)?"HAPTIC ON":"HAPTIC OFF",14,185,170);		
-		 //clear
-		this.g.setColor(0,0);
-		this.g.drawLine (0,98,239,98);
-		this.g.drawLine (0,99,239,99);
-        this.g.flip();
-		this.g.drawLine (120,0,120,195);
-      	this.g.drawLine (121,0,121,195);
-        this.g.flip();		
+		this.g.setColor(0,col("black"));
+		this.g.fillRect(0,196,239,204);
+		this.g.setColor(1,col("lgray"));
+      	this.g.fillRect(75,200,165,204);
+		this.g.flip();
+        this.g.setColor(1,col("white"));
+      	this.g.fillRect(105,200,135,204);
+		this.g.flip(); 
+        this.btn(euc.dash.aLck,"AUTO",18,60,15,col("red"),col("dgray"),0,0,119,97,"LOCK",28,60,50);
+        this.btn((euc.dash.hapS||euc.dash.hapA||euc.dash.hapT||euc.dash.hapB),"HAPTIC",25,185,37,col("raf"),col("raf4"),122,0,239,97);		
+        this.btn(euc.dash.aOff,"AUTO",18,60,115,col("red"),col("dgray"),0,100,119,195,"OFF",30,60,150);
+        this.btn(euc.dash.horn,"HORN",25,185,136,col("raf"),col("raf4"),122,100,239,195);		
         this.run=true;
 	},
 	show : function(){
@@ -54,12 +54,19 @@ face[0] = {
 			this.ntid=setTimeout(function(t){
                 t.ntid=0;
 				t.g.setColor(0,0);
-				t.g.fillRect(0,196,239,239);
+				t.g.fillRect(0,205,239,239);
 				t.g.setColor(1,col("white"));
 				t.g.setFont("Vector",20);
-		        t.g.drawString("HAPTIC ALERTS",120-(t.g.stringWidth("HAPTIC ALERTS")/2),217); 
+		        t.g.drawString("OPTIONS",120-(t.g.stringWidth("OPTIONS")/2),217); 
 				t.g.flip();
-
+				t.g.setColor(0,col("black"));
+				t.g.fillRect(0,196,239,204);
+				t.g.setColor(1,col("lgray"));
+				t.g.fillRect(75,200,165,204);
+				t.g.flip();
+				t.g.setColor(1,col("white"));
+				t.g.fillRect(105,200,135,204);
+				t.g.flip(); 
 			},1000,this);
     },
 	tid:-1,
@@ -83,11 +90,8 @@ face[1] = {
 		return true;
 	},
 	show : function(){
-		if (euc.state=="READY"&&euc.dash.maker=="Kingsong")
-			face.go("dashSetKsOpt",0);
-		else
-			face.go(set.dash[set.def.dash],0);
-		return;
+		face.go("dashSetKingsong",0);
+		return true;
 	},
 	clear: function(){
 		return true;
@@ -96,82 +100,27 @@ face[1] = {
 //touch
 touchHandler[0]=function(e,x,y){ 
 	switch (e) {
-	case 5: //tap event
-		if (face[0].set) { 
+      case 5: //tap event
+		if (x<=120&&y<100) { //auto lock
+			euc.dash.aLck=1-euc.dash.aLck;
+            face[0].btn(euc.dash.aLck,"AUTO",18,60,15,col("red"),col("dgray"),0,0,119,97,"LOCK",28,60,50);
+            face[0].ntfy("DISCONNECT -> LOCK","AUTO LOCK DISABLED",18,col("dgray"),euc.dash.aLck);
 			digitalPulse(D16,1,[30,50,30]);
-			if (face[0].set=="spd") { 
-				if (y<=120){ //spd
-					if (euc.dash.haSv=="spd1") euc.dash.haSv="spd2";
-					else if (euc.dash.haSv=="spd2") euc.dash.haSv="spd3";
-					else euc.dash.haSv="spd1";
-					face[0].btn(1,"<      SPEED      >",20,120,20,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv]+" Km/h",25,120,60);
-					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
-				}else{ //step
-					if (x<=120){ if (1<euc.dash.spdS) euc.dash.spdS--;
-					}else if (euc.dash.spdS<5) euc.dash.spdS++;
-					face[0].btn(1,"<       STEP       >",20,120,120,col("dgray"),0,0,100,239,195,euc.dash.spdS+ " km/h",25,120,160);
-					face[0].ntfy("ONE PULSE PER:","",18,col("dgray"),1);
-				}  
-			}else if (face[0].set=="amp") { //amp
-				if (y<=65){ //uphill
-					if (120<=x&&euc.dash.ampH<35) euc.dash.ampH++;
-					else if (x<=120&&15<euc.dash.ampH) euc.dash.ampH--;
-					face[0].btn(1," UPHILL:",20,80,20,col("dgray"),0,0,0,239,63,euc.dash.ampH+" A",28,200,20);
-					face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);
-				}else if (65<=y&&y<=133){//braking
-					if (x<=120&&euc.dash.ampL<-5) euc.dash.ampL++;
-					else if (120<=x&&-15<euc.dash.ampL) euc.dash.ampL--;
-					face[0].btn(1,"BRAKING:",20,80,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",28,200,90);
-					face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);                    
-				}else {//step
-					if (120<=x&&euc.dash.ampS<3) euc.dash.ampS++;
-					else if (x<=120&&1<euc.dash.ampS) euc.dash.ampS--;
-					face[0].btn(1,"STEP:",20,80,160,col("dgray"),0,0,135,239,195,euc.dash.ampS+ " A",28,200,160);
-					face[0].ntfy("ONE PULSE PER:","",18,col("dgray"),1);
-				}
-            }else if (face[0].set=="temp") { //temp
-              if (y<=120){ //
-					if (120<=x&&euc.dash.tmpH<70) euc.dash.tmpH++;
-   			  		else if (x<=120&&50<euc.dash.tmpH) euc.dash.tmpH--;
-                    face[0].btn(1,"SET TEMPERATURE:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,32,120,55);
-					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
-				}else{ //back
-                  face[0].set=0;face[0].init();
-                }
-            }else if (face[0].set=="bat") { //temp
-              if (y<=120){ //
-					if (120<=x&&euc.dash.batL<30) euc.dash.batL++;
-   			  		else if (x<=120&&5<euc.dash.batL) euc.dash.batL--;
-                    face[0].btn(1,"SET BATTERY:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,32,120,55);
-					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
-				}else{ //back
-                  face[0].set=0;face[0].init();
-                }
-              
-			}else  {digitalPulse(D16,1,40);face[0].set=0;face[0].init();}
-        }else{
-			if (x<=120&&y<100) { //Speed
-				euc.dash.hapS=1-euc.dash.hapS;
-				face[0].btn(euc.dash.hapS,"SPEED",24,60,20,col("blue1"),col("raf4"),0,0,119,97,(euc.dash.hapS)?"HAPTIC ON":"HAPTIC OFF",14,60,70);
-				face[0].ntfy("HOLD -> SET SPEED","HOLD -> SET SPEED",18,col("dgray"),1);
-				digitalPulse(D16,1,[30,50,30]);
-			}else if (120<=x&&y<=100) { //Ampere
-				euc.dash.hapA=1-euc.dash.hapA;
-				face[0].btn(euc.dash.hapA,"AMPERE",24,185,20,col("blue1"),col("raf4"),122,0,239,97,(euc.dash.hapA)?"HAPTIC ON":"HAPTIC OFF",14,185,70);
-				face[0].ntfy("HOLD -> SET AMPERE","",18,col("dgray"),1);
-				digitalPulse(D16,1,[30,50,30]);
-			}else if (x<=120&&100<=y) { //Temp
-				euc.dash.hapT=1-euc.dash.hapT;
-				face[0].btn(euc.dash.hapT,"TEMP",24,60,120,col("blue1"),col("raf4"),0,100,119,195,(euc.dash.hapT)?"HAPTIC ON":"HAPTIC OFF",14,60,170);
-				face[0].ntfy("HOLD -> SET TEMP","",18,col("dgray"),1);
-				digitalPulse(D16,1,[30,50,30]);		
-			}else if (120<=x&&100<=y) { //Batt
-				euc.dash.hapB=1-euc.dash.hapB;
-				face[0].btn(euc.dash.hapB,"BATT",24,185,120,col("blue1"),col("raf4"),122,100,239,195,(euc.dash.hapB)?"HAPTIC ON":"HAPTIC OFF",14,185,170);
-				face[0].ntfy("HOLD -> SET BATTERY","",18,col("dgray"),1);
-				digitalPulse(D16,1,[30,50,30]);						
-			}else digitalPulse(D16,1,[30,50,30]);
-        }
+		}else if (120<=x<=239&&y<=100) { //buzz
+			digitalPulse(D16,1,[30,50,30]);						
+			face.go("dashAlerts",0);
+			return;		
+		}else if (x<=120&&100<=y<=200) { //auto off
+			euc.dash.aOff=1-euc.dash.aOff;
+            face[0].btn(euc.dash.aOff,"AUTO",18,60,115,col("red"),col("dgray"),0,100,119,195,"OFF",30,60,150);
+            face[0].ntfy("DISCONNECT->POWER OFF","AUTO OFF DISABLED",16,col("dgray"),euc.dash.aOff);
+			digitalPulse(D16,1,[30,50,30]);		
+		}else if (120<=x<=239&&100<=y<=200) { //lock
+			euc.dash.horn=1-euc.dash.horn;
+            face[0].btn(euc.dash.horn,"HORN",25,185,136,col("raf"),col("raf4"),122,100,239,195);
+            face[0].ntfy("BUTTON IS HORN >2KPH","HORN DISABLED",18,col("dgray"),euc.dash.horn);
+			digitalPulse(D16,1,[30,50,30]);						
+		}else digitalPulse(D16,1,[30,50,30]);
 		this.timeout();
 		break;
 	case 1: //slide down event
@@ -189,45 +138,34 @@ touchHandler[0]=function(e,x,y){
 		this.timeout();
 		break;
 	case 3: //slide left event
-		digitalPulse(D16,1,40);
-		break;
+		face.go("dashSetKsAdv",0);
+		return;
 	case 4: //slide right event (back action)
-        if (face[0].set) {
-  			face[0].set=0;face[0].init();
-        }else if (euc.state=="READY"&&euc.dash.maker=="Kingsong")
-			face.go("dashSetKsOpt",0);
-		else
-			face.go(set.dash[set.def.dash],0);
+		face.go("dashSetKingsong",0);
 		return;
 	case 12: //hold event
-		if (face[0].set) { 
-			face[0].set=0;face[0].init();
-			digitalPulse(D16,1,[30,50,30]);	
-        }else if (x<=120&&y<100) { //spd
-			face[0].set="spd";
-            digitalPulse(D16,1,[30,50,30]);
-            face[0].btn(1,"<      SPEED      >",20,120,20,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv]+" Km/h",25,120,60);
-			face[0].btn(1,"<       STEP       >",20,120,120,col("dgray"),0,0,100,239,195,euc.dash.spdS+ " km/h",25,120,160);
-		}else if (120<=x&&y<=100) { //amp
-			face[0].set="amp";
+		if (x<=120&&y<100) { //auto lock
+			euc.dash.aLck=1-euc.dash.aLck;
+            face[0].btn(euc.dash.aLck,"AUTO",18,60,15,col("red"),col("dgray"),0,0,119,97,"LOCK",28,60,50);
+            face[0].ntfy("DISCONNECT -> LOCK","AUTO LOCK DISABLED",18,col("dgray"),euc.dash.aLck);
 			digitalPulse(D16,1,[30,50,30]);
-            w.gfx.setColor(0,0);
-	    	w.gfx.fillRect(0,0,239,195);
-    		w.gfx.flip();
-            face[0].btn(1," UPHILL:",20,80,20,col("dgray"),0,0,0,239,63,euc.dash.ampH+" A",28,200,20);
-			face[0].btn(1,"BRAKING:",20,80,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",28,200,90);
-			face[0].btn(1,"STEP:",20,80,160,col("dgray"),0,0,135,239,195,euc.dash.ampS+ " A",28,200,160);
-		}else if (x<=120&&100<=y) { //temp
-			face[0].set="temp";
-            digitalPulse(D16,1,[30,50,30]);
-            face[0].btn(1,"SET TEMPERATURE:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,32,120,55);
-			face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);                    
-		}else if (x<=120&&100<=y) { //bat
-			face[0].set="bat";
-            digitalPulse(D16,1,[30,50,30]);
-            face[0].btn(1,"SET BATTERY:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,32,120,55);
-			face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);                    
-		}else digitalPulse(D16,1,[30,50,30]);		
+		}else if (120<=x<=239&&y<=100) { //buzz
+			if (euc.dash.hapS||euc.dash.hapA||euc.dash.hapT||euc.dash.hapB) {euc.dash.hapS=0;euc.dash.hapA=0;euc.dash.hapT=0;euc.dash.hapB=0;}
+			else {euc.dash.hapS=1;euc.dash.hapA=1;euc.dash.hapT=1;euc.dash.hapB=1;}
+            face[0].btn((euc.dash.hapS||euc.dash.hapA||euc.dash.hapT||euc.dash.hapB),"HAPTIC",25,185,37,col("raf"),col("raf4"),122,0,239,97);
+            face[0].ntfy("HAPTIC ENABLED","HAPTIC DISABLED",19,col("dgray"),(euc.dash.hapS||euc.dash.hapA||euc.dash.hapT||euc.dash.hapB));
+			digitalPulse(D16,1,[30,50,30]);
+		}else if (x<=120&&100<=y<=200) { //auto off
+			euc.dash.aOff=1-euc.dash.aOff;
+            face[0].btn(euc.dash.aOff,"AUTO",18,60,115,col("red"),col("dgray"),0,100,119,195,"OFF",30,60,150);
+            face[0].ntfy("DISCONNECT->POWER OFF","AUTO OFF DISABLED",16,col("dgray"),euc.dash.aOff);
+			digitalPulse(D16,1,[30,50,30]);		
+		}else if (120<=x<=239&&100<=y<=200) { //lock
+			euc.dash.horn=1-euc.dash.horn;
+            face[0].btn(euc.dash.horn,"HORN",25,185,136,col("raf"),col("raf4"),122,100,239,195);
+            face[0].ntfy("BUTTON IS HORN >2KPH","HORN DISABLED",18,col("dgray"),euc.dash.horn);
+			digitalPulse(D16,1,[30,50,30]);						
+		}else digitalPulse(D16,1,[30,50,30]);
 		this.timeout();
 		break;
   }
