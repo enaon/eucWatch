280349ed6744104c238335ed9261e9f4fbf67811
enaon <enaon@enaon>
2021-03-10 03:24:55 +0200

    Update eucKingsong.js

--- P8-testing/eucKingsong/eucKingsong.js
+++ P8-testing/eucKingsong/eucKingsong.js
@@ -350,15 +350,13 @@ euc.off=function(err){
 	} else {
 		if (set.def.cli) console.log("EUC OUT:",err);
 			//global["\xFF"].bleHdl=[];
-			euc.dash.aOff=euc.aOff;
-			euc.dash.aLck=euc.aLck;
+			if ( euc.aOff) {euc.dash.aOff=euc.aOff;	delete euc.aOff;}
+			if ( euc.aLck) {euc.dash.aLck=euc.aLck;	delete euc.aLck;}
 			clearTimeout(euc.busy);euc.busy=0;
 			euc.off=function(){if (set.def.cli) console.log("EUC allready killed at:",err);};
 			delete euc.conn;
 			delete euc.wri;
 			delete euc.cmd;
-			delete euc.aoff;
-			delete euc.aLck;
             euc.run=0;
 			NRF.setTxPower(set.def.rfTX);	
     }
