ea4ea21df3b57c7d06414e6951b9baf6055b4561
enaon <enaon@enaon>
2021-04-05 03:39:44 +0300

    Update handler.js

--- DSD6/handler/handler.js
+++ DSD6/handler/handler.js
@@ -46,97 +46,76 @@ function buttonHandler(s){
 	}
 }
 setWatch(buttonHandler,BTN1, {repeat:true, debounce:10,edge:"both"});
-//accel
-/*
-w.initAccel();
-//reset
-w.accWrite(0x7f,0x00); 
-w.accWrite(0x19,0x00); 
-w.accWrite(0x19,0x80);
-//w.accRead(0x0C,1); //=0x55(85)
-//
-//w.accWrite(0x18,0x40); //standby mode-high resolution
-//w.accWrite(0x18,0x60); //standby mode-high resolution -interrupt
-w.accWrite(0x18,0x41); //standby mode
-//w.accWrite(0x18,0x44); //standby mode- high resolution -2g- tap
-//w.accWrite(0x18,0x45); //both tilt and tap
-//w.accWrite(0x1A,0x06); //wake up 50hz
-//w.accWrite(0x1A,0x98); //tap 400hz
-
-
-//w.accWrite(0x19,0x3F); //all three axes
-w.accWrite(0x19,0x10);  //enable when face up
-w.accWrite(0x22,0x01);
-//w.accWrite(0x23,0x01); //motion time 0.1 if 50hz
-//w.accWrite(0x24,0x03); //single and double tap (default)
-//w.accWrite(0x25,0x78); //double tap  0.3sec (default)
-//w.accWrite(0x30,0x08); //int wake up 0.5g
-w.accWrite(0x32,0x05); //22 degrees
-//w.accWrite(0x32,0x03); //6 degrees
-w.accWrite(0x1c,0x30); // outputoutput physical interrupt ot tap
-//w.accWrite(0x1c,0x38); //enable int1-polarity high-pulse 0.03-0.05
-//w.accWrite(0x1D,0x7F); //direction of motion all
-//w.accWrite(0x1E,0x3F); //direction of tap all
-w.accWrite(0x1f,0x01);
-//w.accWrite(0x1f,0x02); //report wake up on int1
-
-//w.accWrite(0x1f,0x40); //buffer full in int1
-
-//w.accWrite(0x1f,0x10); //data ready report on int1
-//w.accWrite(0x1f,0x04); //tap report on int1
-//w.accWrite(0x1f,0x05); //both tilt and tap report on int1
-//w.accWrite(0x1B,0x02); // odr to 50hz (default)
-//w.accWrite(0x18,0x42); //standby mode-high resolution-+2g-wake up
-//w.accWrite(0x18,0x00); // standy-op mode low-+2g-acc pc1=1
-//w.accWrite(0x18,0xE0); // operational mode acc pc1=1
-//w.accWrite(0x18,0xC0); // operational mode pc1=1
-//w.accWrite(0x18,0x80); // operational mode acc pc1=1
-//w.accWrite(0x18,0xC1); // operational mode tilt
-//w.accWrite(0x18,0xC4); // operational mode tap
-//w.accWrite(0x18,0xC5); // operational mode tilt and tap
-
-*/
-
+//acc
+function tilt(i){print(i);}
+function tap(i,o){print(i,o);}
 var acc={
-	curr:"main",
-	offms:-1,
-	on:function(){
+	mode:0,
+	on:function(i){
+		if (acc.wtc) {clearWatch(acc.wtc);acc.wtc=0;}
 		w.initAccel();
 		w.accWrite(0x7f,0x00); 
 		w.accWrite(0x19,0x00); 
 		w.accWrite(0x19,0x80);
-		w.accWrite(0x18,0x44); //standby mode-low current-int1-2g-tap-wake-notilt
-		w.accWrite(0x18,0xC4); //standby mode-low current-int1-2g-tap-wake-notilt
-		w.accWrite(0x1f,0x04); //tap report on int1
-		w.accWrite(0x1c,0x30); //enable int1
+		if (i==2) {
+			this.mode=i;
+			w.accWrite(0x18,0x45); //standby mode-high current-int1-2g-tap-tilt
+			w.accWrite(0x22,0x01); //tilt
+			w.accWrite(0x19,0x10);
+			w.accWrite(0x1f,0x05); //tap-tilt report on int1
+			w.accWrite(0x1c,0x30); //enable int1
+			w.accWrite(0x18,0xC5); //opp mode-high current-int1-2g-tap-tilt-enable
+		}else if (i==1){
+			this.mode=i;
+//			w.accWrite(0x18,0x44); //standby mode-high current-int1-2g-tap
+			w.accWrite(0x18,0x4C); //standby mode-high current-int1-4g-tap
+			w.accWrite(0x18,0x5C); //standby mode-high current-int1-8g-tap
+			w.accWrite(0x1f,0x04); //tap report on int1
+			w.accWrite(0x1c,0x30); //enable int1
+//			w.accWrite(0x18,0xC4); //opp mode-high current-int1-2g-tap
+//			w.accWrite(0x18,0xCC); //opp mode-high current-int1-4g-tap
+			w.accWrite(0x18,0xDC); //opp mode-high current-int1-8g-tap
+
+		} else {
+			this.mode=0;
+			w.accWrite(0x18,0x41); //standby mode-high current-int1-2g-tilt
+			w.accWrite(0x22,0x01); //tilt
+			w.accWrite(0x19,0x10);
+			w.accWrite(0x1f,0x01); //tilt report on int1
+			w.accWrite(0x1c,0x30); //enable int1
+			w.accWrite(0x18,0xC1); //opp mode-high current-int1-2g-tilt
+		}		
 		acc.wtc=setWatch(function(s){
 			if (s.state){
-				console.log((w.accRead(0,10)));
-				console.log((w.accRegDump(0x10)));
-				var state=(w.accRegDump(0x10)).toString().split(' ');
-				if (state[0]==16) {
-					// w.accWrite(0x19,0x2f);
-					//w.accWrite(0x19,0x2c);
-					w_up=true;
-					print(1);
+				console.log((w.accRead(17,3)));
+				let state=w.accRead(17,3);
+				if (state[2]==1) {
+					if (state[0]==32||state[0]==4||state[0]==8) {
+						w.accWrite(0x19,0x2c);
+						tilt("up");
+					}else {
+						w.accWrite(0x19,0x10);
+						tilt("dn");
+					}
 				}else {
-					//w.accWrite(0x19,0x10);
-					w_up=false;
-					print(2);
-				}
-				w.accRegDump(0x17);
+					if  (state[2]==4) {
+						tap("single",state[1]);
+					}else
+						tap("double",state[1]);
+					}
 			}
+			w.accRegDump(0x17);
 		},D15,true);  
 	},
 	off:function(app){
 		if (acc.wtc) {clearWatch(acc.wtc);acc.wtc=0;}
 		w.accWrite(0x7f,0x00); 
+		w.accWrite(0x18,0x00); 
 		w.accWrite(0x19,0x00); 
 		w.accWrite(0x19,0x80);
 //		w.accWrite(0x1A,0x00); //cntrl3 slow
 	}
 };
-
 //charge pin
 setWatch(function(s){
 		if (digitalRead(D2)==1) 
@@ -151,6 +130,3 @@ setTimeout(()=>{
 	//o.off();
 	o.setRotation(0); //vertical
 },500);
-
-
-
