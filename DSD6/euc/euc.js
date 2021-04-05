85046f66ca177ff8baca96963298618b3d1ae565
enaon <enaon@enaon>
2021-04-05 05:10:07 +0300

    up

--- DSD6/euc/euc.js
+++ DSD6/euc/euc.js
@@ -12,12 +12,11 @@ global.euc= {
 	updateDash:function(slot){require('Storage').write('eucSlot'+slot+'.json', euc.dash);},
 	tgl:function(){ 
 		if (this.state!="OFF" ) {
-			digitalPulse(D16,1,[90,60,90]);  
-			set.def.accE=0;
+/			set.def.accE=0;
 			if (!set.def.acc) {acc.off();}
 			this.seq=1;
 			this.state="OFF";
-   			face.go(set.dash[set.def.dash],0);
+   			face.go("dash"],0);
 			//if (this.kill) clearTimout(this.kill);
 			//this.kill=setTimeout(()=>{
 			//if (euc.dash.emu) {set.def.atc=0;set.upd();}
@@ -30,18 +29,18 @@ global.euc= {
 			return;
 		}else {
 			NRF.setTxPower(4);
-			digitalPulse(D16,1,100); 
-			//if (euc.dash.emu){set.def.atc=1;set.def.gb=0;set.def.cli=0;set.def.hid=0;set.upd();}
+/			//if (euc.dash.emu){set.def.atc=1;set.def.gb=0;set.def.cli=0;set.def.hid=0;set.upd();}
 			this.mac=require("Storage").readJSON("dash.json",1)["slot"+require("Storage").readJSON("dash.json",1).slot+"Mac"];
 			if(!this.mac) {
-				face.go('dashScan',0);return;
+				print("nomac");
+			//	face.go('dashScan',0);return;
 		    }else {
 				eval(require('Storage').read('euc'+require("Storage").readJSON("dash.json",1)["slot"+require("Storage").readJSON("dash.json",1).slot+"Maker"]));
 				this.state="ON";
 				if (!set.def.acc) {set.def.accE=1;acc.on();}
 				this.seq=1;
 				this.conn(this.mac); 
-				face.go(set.dash[set.def.dash],0);return;
+				face.go("dash");return;
             }
 		}
 	} 
