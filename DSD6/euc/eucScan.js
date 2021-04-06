9f79c0ed6fa6f5995b7ccabef760d82f1180391e
enaon <enaon@enaon>
2021-04-06 05:31:37 +0300

    up

--- DSD6/euc/eucScan.js
+++ DSD6/euc/eucScan.js
@@ -1,30 +1,31 @@
 //scan 
 if(!global.scan){
-scan={
-    mac:[],
-	go:function(app,service){
-		set.gIsB=1;
-		NRF.findDevices(function(devices) {
-		  this.slot="";
-		  app="dash";
-		  if (euc.dash.maker=="NinebotZ")  this.filter = [{manufacturer:16974}];  
-		  else if (euc.dash.maker=="InmotionV11")  this.filter = [{ namePrefix: 'V11-' }]
-		  else this.filter = [{services:[service]}];
-		  var found=[];
-		  NRF.filterDevices(devices, this.filter).forEach(function(entry) {found.push(entry.id);});
-		  if (found!=""&&found!=undefined){ 
-			if (app=="dash"){
-				set.write("dash","slot"+require("Storage").readJSON("dash.json",1).slot+"Mac",found[0]+"");
-				euc.dash.mac=found[0]+"";
-			}else{
-				set.write("setting",app+"Mac",found);
-				set.write("setting",app+"_go","0");
-			}
-			scan.mac=found;
-		  } else scan.mac=[];
-		  set.gIsB=0;
-		  face[0].start=1;
-		  //if (face.appCurr!="w_scan") {delete scan.go;delete scan;}
-		}, 2000);
-	}	
-};
+  scan={
+      mac:[],
+    go:function(app,service){
+      set.gIsB=1;
+      NRF.findDevices(function(devices) {
+        this.slot="";
+        app="dash";
+        if (euc.dash.maker=="NinebotZ")  this.filter = [{manufacturer:16974}];  
+        else if (euc.dash.maker=="InmotionV11")  this.filter = [{ namePrefix: 'V11-' }];
+        else this.filter = [{services:[service]}];
+        var found=[];
+        NRF.filterDevices(devices, this.filter).forEach(function(entry) {found.push(entry.id);});
+        if (found!=""&&found!=undefined){ 
+        if (app=="dash"){
+          set.write("dash","slot"+require("Storage").readJSON("dash.json",1).slot+"Mac",found[0]+"");
+          euc.dash.mac=found[0]+"";
+        }else{
+          set.write("setting",app+"Mac",found);
+          set.write("setting",app+"_go","0");
+        }
+        scan.mac=found;
+        } else scan.mac=[];
+        set.gIsB=0;
+        face[0].start=1;
+        //if (face.appCurr!="w_scan") {delete scan.go;delete scan;}
+      }, 2000);
+    }	
+  };
+}
