6cdd2aa62462fdd4f1b3532e2a0410ff0d49d74b
enaon <enaon@enaon>
2021-05-03 03:33:38 +0300

    fix voltage

--- P8-testing/eucInmotion/eucInmotion.js
+++ P8-testing/eucInmotion/eucInmotion.js
@@ -108,7 +108,7 @@ euc.conn=function(mac){
 				euc.dash.batC = (euc.dash.batH <= euc.dash.bat)? 0 : (euc.dash.batM <= euc.dash.bat)? 1 : (euc.dash.batL <= euc.dash.bat)? 2 : 3;	
 				if ( euc.dash.hapB && euc.dash.bat <= euc.dash.batL ) { euc.alert ++; euc.dash.spdC = 3; }    
 				//trip 
-				euc.dash.trpL=(data.getUint16(17, true))*10; //trip
+				euc.dash.trpL=(data.getUint16(17, true))/10; //trip
 				//euc.dash.trpL=(data.getUint16(19, true))*10; //remain
 				//euc.dash.time=(data.getUint16(7, true)/60)|0;
 				//temp
