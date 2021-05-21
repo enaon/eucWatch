335dc3d81d1586c00f7ef3a244ba8cb636cc450f
enaon <enaon@enaon>
2021-05-21 13:24:25 +0300

    ninebot S init

---
+++ P8-testing/dashNinebotS/dashNinebotZ.js
@@ -0,0 +1,250 @@
+//dash  Alerts
+face[0] = {
+	offms: 5000,
+	g:w.gfx,
+	init: function(){
+		if (euc.state!=="READY"&&face.appPrev!=="dashGarage") {face.go(set.dash[set.def.dash],0);return;}
+        if (!face.appPrev.startsWith("dash")) this.g.clear();
+        this.g.setColor(0,0);
+		this.g.fillRect(0,0,239,239);
+		this.g.setColor(1,col("white"));
+		this.g.setFont("Vector",20);
+		this.g.drawString("HAPTIC ALERTS",120-(this.g.stringWidth("HAPTIC ALERTS")/2),217); 
+		this.g.flip(); 
+		this.btn(euc.dash.hapS,"SPEED",25,60,37,col("blue1"),col("raf4"),0,0,119,97);
+		this.btn(euc.dash.hapA,"AMP",25,185,37,col("blue1"),col("raf4"),122,0,239,97);
+		this.btn(euc.dash.hapT,"TEMP",25,60,136,col("blue1"),col("raf4"),0,100,119,195);
+        this.btn(euc.dash.hapB,"BATT",25,185,136,col("blue1"),col("raf4"),122,100,239,195);			
+        this.run=true;
+	},
+	show : function(){
+		if (euc.state!=="READY"&&face.appPrev!=="dashGarage") {face.go(set.dash[set.def.dash],0);return;}
+		if (!this.run) return; 
+        this.tid=setTimeout(function(t,o){
+		  t.tid=-1;
+		  t.show();
+        },1000,this);
+	},
+    btn: function(bt,txt1,size1,x1,y1,clr1,clr0,rx1,ry1,rx2,ry2,txt2,size2,x2,y2){
+			this.g.setColor(0,(bt)?clr1:clr0);
+			this.g.fillRect(rx1,ry1,rx2,ry2);
+			this.g.setColor(1,col("white"));
+			this.g.setFont("Vector",size1);	
+          this.g.drawString(txt1,x1-(this.g.stringWidth(txt1)/2),y1); 
+   			if (txt2){this.g.setFont("Vector",size2);	
+            this.g.drawString(txt2,x2-(this.g.stringWidth(txt2)/2),y2);}
+			this.g.flip();
+    },
+    ntfy: function(txt1,txt0,size,clr,bt){
+            this.g.setColor(0,clr);
+			this.g.fillRect(0,198,239,239);
+			this.g.setColor(1,col("white"));
+			this.g.setFont("Vector",size);
+     		this.g.drawString((bt)?txt1:txt0,120-(this.g.stringWidth((bt)?txt1:txt0)/2),214); 
+			this.g.flip();
+			if (this.ntid) clearTimeout(this.ntid);
+			this.ntid=setTimeout(function(t){
+                t.ntid=0;
+				t.g.setColor(0,0);
+				t.g.fillRect(0,196,239,239);
+				t.g.setColor(1,col("white"));
+				t.g.setFont("Vector",20);
+		        t.g.drawString("HAPTIC ALERTS",120-(t.g.stringWidth("HAPTIC ALERTS")/2),217); 
+				t.g.flip();
+
+			},1000,this);
+    },
+	tid:-1,
+	run:false,
+	clear : function(){
+		//this.g.clear();
+		this.run=false;
+		if (this.tid>=0) clearTimeout(this.tid);this.tid=-1;
+   		if (this.ntid) clearTimeout(this.ntid);this.ntid=0;
+		return true;
+	},
+	off: function(){
+		this.g.off();
+		this.clear();
+	}
+};
+//loop face
+face[1] = {
+	offms:1000,
+	init: function(){
+		return true;
+	},
+	show : function(){
+		face.go(set.dash[set.def.dash],0);
+		return;
+	},
+	clear: function(){
+		return true;
+	},
+};	
+//touch
+touchHandler[0]=function(e,x,y){ 
+	switch (e) {
+	case 5: //tap event
+		if (face[0].set) { 
+			digitalPulse(D16,1,[30,50,30]);
+			if (face[0].set=="spd") { 
+				if (y<=120){ //spd
+					if (require("Storage").readJSON("dash.json",1)["slot"+require("Storage").readJSON("dash.json",1).slot+"Maker"]!="Kingsong") {
+						if (x<=120){ if (15<euc.dash.spd1) euc.dash.spd1--;
+						}else if (euc.dash.spd1<45) euc.dash.spd1++;
+                        euc.dash.haSv="spd1";
+					}else {
+						if (euc.dash.haSv=="spd1") euc.dash.haSv="spd2";
+						else if (euc.dash.haSv=="spd2") euc.dash.haSv="spd3";
+						else euc.dash.haSv="spd1";
+					}
+                    face[0].btn(1,"SPEED (IN Km/h)",18,120,8,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv],50,120,40);
+					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
+				}else{ //RESOLUTION
+					if (x<=120){ if (1<euc.dash.spdS) euc.dash.spdS--;
+					}else if (euc.dash.spdS<5) euc.dash.spdS++;
+			        face[0].btn(1,"RESOLUTION (IN Km/h)",18,120,110,col("gray"),0,0,100,239,195,euc.dash.spdS,50,120,140);
+					face[0].ntfy("ONE PULSE PER:","",18,col("dgray"),1);
+				}  
+			}else if (face[0].set=="amp") { //amp
+				if (y<=65){ //uphill
+					if (120<=x&&euc.dash.ampH<35) euc.dash.ampH++;
+					else if (x<=120&&15<euc.dash.ampH) euc.dash.ampH--;
+                    face[0].btn(1,"UPHILL:",20,60,23,col("gray"),0,0,0,239,63,euc.dash.ampH+" A",35,180,16);
+					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
+				}else if (65<=y&&y<=133){//braking
+					if (x<=120&&euc.dash.ampL<-5) euc.dash.ampL++;
+					else if (120<=x&&-15<euc.dash.ampL) euc.dash.ampL--;
+			        face[0].btn(1,"BRAKING:",20,60,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",35,182,84);
+					face[0].ntfy("HAPTIC STARTS AT::","",18,col("dgray"),1);                    
+				}else {//RESOLUTION
+					if (120<=x&&euc.dash.ampS<3) euc.dash.ampS++;
+					else if (x<=120&&1<euc.dash.ampS) euc.dash.ampS--;
+			        face[0].btn(1,"RESOLUTION:",17,70,157,col("gray"),0,0,135,239,195,euc.dash.ampS+ " A",35,190,150);
+					face[0].ntfy("ONE PULSE PER:","",18,col("dgray"),1);
+				}
+            }else if (face[0].set=="temp") { //temp
+              if (y<=120){ //
+					if (120<=x&&euc.dash.tmpH<70) euc.dash.tmpH++;
+   			  		else if (x<=120&&50<euc.dash.tmpH) euc.dash.tmpH--;
+                    face[0].btn(1,"SET HI-TEMP:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,50,120,41);
+					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
+				}else{ //back
+		            w.gfx.setColor(0,0);
+					w.gfx.drawLine (120,0,120,97);
+					w.gfx.drawLine (121,0,121,97);
+					w.gfx.flip();
+					face[0].set=0;face[0].init();
+                }
+            }else if (face[0].set=="batt") { //temp
+              if (y<=120){ //
+					if (120<=x&&euc.dash.batL<30) euc.dash.batL++;
+   			  		else if (x<=120&&5<euc.dash.batL) euc.dash.batL--;
+                    face[0].btn(1,"SET LOW-BATT:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,50,120,41);
+					face[0].ntfy("HAPTIC STARTS AT:","",18,col("dgray"),1);
+				}else{ //back
+           		    w.gfx.setColor(0,0);
+					w.gfx.drawLine (120,0,120,97);
+					w.gfx.drawLine (121,0,121,97);
+					w.gfx.flip();
+					face[0].set=0;face[0].init();
+                }
+              
+			}else  {digitalPulse(D16,1,40);face[0].set=0;face[0].init();}
+        }else{
+			if (x<=120&&y<100) { //Speed
+				euc.dash.hapS=1-euc.dash.hapS;
+				face[0].btn(euc.dash.hapS,"SPEED",25,60,37,col("blue1"),col("raf4"),0,0,119,97);
+				face[0].ntfy("HOLD -> SET SPEED","HOLD -> SET SPEED",18,col("dgray"),1);
+				digitalPulse(D16,1,[30,50,30]);
+			}else if (120<=x&&y<=100) { //Ampere
+				euc.dash.hapA=1-euc.dash.hapA;
+				face[0].btn(euc.dash.hapA,"AMP",25,185,37,col("blue1"),col("raf4"),122,0,239,97);
+				face[0].ntfy("HOLD -> SET AMPERE","",18,col("dgray"),1);
+				digitalPulse(D16,1,[30,50,30]);
+			}else if (x<=120&&100<=y) { //Temp
+				euc.dash.hapT=1-euc.dash.hapT;
+				face[0].btn(euc.dash.hapT,"TEMP",25,60,136,col("blue1"),col("raf4"),0,100,119,195);
+				face[0].ntfy("HOLD -> SET TEMP","",18,col("dgray"),1);
+				digitalPulse(D16,1,[30,50,30]);		
+			}else if (120<=x&&100<=y) { //Batt
+				euc.dash.hapB=1-euc.dash.hapB;
+				face[0].btn(euc.dash.hapB,"BATT",25,185,136,col("blue1"),col("raf4"),122,100,239,195);
+				face[0].ntfy("HOLD -> SET BATTERY","",18,col("dgray"),1);
+				digitalPulse(D16,1,[30,50,30]);						
+			}else digitalPulse(D16,1,[30,50,30]);
+        }
+		this.timeout();
+		break;
+	case 1: //slide down event
+		//face.go("main",0);
+		face.go(set.dash[set.def.dash],0);
+		return;	 
+	case 2: //slide up event
+		if (y>200&&x<50) { //toggles full/current brightness on a left down corner swipe up. 
+			if (w.gfx.bri.lv!==7) {this.bri=w.gfx.bri.lv;w.gfx.bri.set(7);}
+			else w.gfx.bri.set(this.bri);
+			digitalPulse(D16,1,[30,50,30]);
+		}else //if (y>100) {
+			if (Boolean(require("Storage").read("settings"))) {face.go("settings",0);return;}  
+		//} else {digitalPulse(D16,1,40);}
+		this.timeout();
+		break;
+	case 3: //slide left event
+		digitalPulse(D16,1,40);
+		break;
+	case 4: //slide right event (back action)
+        if (face[0].set) {
+       		this.timeout();
+		   //clear
+		    w.gfx.setColor(0,0);
+		    w.gfx.drawLine (0,98,239,98);
+		    w.gfx.drawLine (0,99,239,99);
+            w.gfx.flip();
+		    w.gfx.drawLine (120,0,120,195);
+          	w.gfx.drawLine (121,0,121,195);
+            w.gfx.flip();	
+  			face[0].set=0;face[0].init();
+        }else {
+			face.go(set.dash[set.def.dash],0);
+			return;
+		}
+	case 12: //hold event
+		if (face[0].set) { 
+		    w.gfx.setColor(0,0);
+		    w.gfx.drawLine (0,98,239,98);
+		    w.gfx.drawLine (0,99,239,99);
+            w.gfx.flip();
+		    w.gfx.drawLine (120,0,120,195);
+          	w.gfx.drawLine (121,0,121,195);
+            w.gfx.flip();
+			face[0].set=0;face[0].init();
+			digitalPulse(D16,1,[30,50,30]);	
+        }else if (x<=120&&y<100) { //spd
+			face[0].set="spd";
+            digitalPulse(D16,1,[30,50,30]);
+            face[0].btn(1,"SPEED (IN Km/h)",18,120,8,col("dgray"),0,0,0,239,97,euc.dash[euc.dash.haSv],50,120,40);
+			face[0].btn(1,"RESOLUTION (IN Km/h)",18,120,110,col("gray"),0,0,100,239,195,euc.dash.spdS,50,120,140);
+		}else if (120<=x&&y<=100) { //amp
+			face[0].set="amp";
+			digitalPulse(D16,1,[30,50,30]);
+            w.gfx.setColor(0,0);
+	    	w.gfx.fillRect(0,0,239,195);
+    		w.gfx.flip();
+            face[0].btn(1,"UPHILL:",20,60,23,col("gray"),0,0,0,239,63,euc.dash.ampH+" A",35,180,16);
+			face[0].btn(1,"BRAKING:",20,60,90,col("dgray"),0,0,66,239,132,euc.dash.ampL+ " A",35,182,84);
+			face[0].btn(1,"RESOLUTION:",17,70,157,col("gray"),0,0,135,239,195,euc.dash.ampS+ " A",35,190,150);
+		}else if (x<=120&&100<=y) { //temp
+			face[0].set="temp";
+            digitalPulse(D16,1,[30,50,30]);
+            face[0].btn(1,"SET HI-TEMP:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.tmpH,50,120,40);
+		}else if (120<=x&&100<=y) { //batt
+			face[0].set="batt";
+            digitalPulse(D16,1,[30,50,30]);
+            face[0].btn(1,"SET LOW-BATT:",18,120,8,col("dgray"),0,0,0,239,97,euc.dash.batL,50,120,40);
+		}else digitalPulse(D16,1,[30,50,30]);		
+		this.timeout();
+		break;
+  }
+};
---
+++ P8-testing/eucNinebotS/eucNinebotS.js
@@ -0,0 +1,192 @@
+//m_euc ninebot one Z10
+euc.tmp={count:0,loop:0};
+euc.cmd=function(no){
+	switch (no) {
+//    case "live":return [90,165,1,62,20,01,176,32,219,254]; //Current Amperage with sign if v[80] > 32768 I = v[80] - 65536 else I = v[80] in Amperes * 100
+    case "live" :return [0x5a,0xa5,0x01,0x3e,0x14,0x01,0xb0,0x20,0xdb,0xfe]; //Current Amperage with sign if v[80] > 32768 I = v[80] - 65536 else I = v[80] in Amperes * 100
+    case "live1":return [0x5a,0xa5,0x01,0x3e,0x14,0x01,0x68,0x02,0x41,0xff]; //Current Amperage with sign if v[80] > 32768 I = v[80] - 65536 else I = v[80] in Amperes * 100
+    case "live2":return [0x5a,0xa5,0x01,0x3e,0x14,0x01,0x10,0x0e,0x8d,0xff]; //Current Amperage with sign if v[80] > 32768 I = v[80] - 65536 else I = v[80] in Amperes * 100
+    case "live3":return [0x5a,0xa5,0x01,0x3e,0x14,0x01,0x1a,0x02,0x8f,0xff]; //Current Amperage with sign if v[80] > 32768 I = v[80] - 65536 else I = v[80] in Amperes * 100
+    }
+};
+//
+euc.conn=function(mac){
+    if ( global["\xFF"].BLE_GATTS!="undefined") {
+		if (set.def.cli) print("ble allready connected"); 
+		if (global["\xFF"].BLE_GATTS.connected) 
+		global["\xFF"].BLE_GATTS.disconnect();
+		this.tgl();
+		return;
+    }
+	if (euc.reconnect) {clearTimeout(euc.reconnect); euc.reconnect=0;}
+	NRF.connect(mac,{minInterval:7.5, maxInterval:15})
+		.then(function(g) {
+			return g.getPrimaryService("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
+		}).then(function(s) {
+			euc.serv=s;
+			return euc.serv.getCharacteristic("6e400002-b5a3-f393-e0a9-e50e24dcca9e"); // write
+		}).then(function(wc) {
+			euc.wCha=wc;//write
+			return euc.serv.getCharacteristic("6e400003-b5a3-f393-e0a9-e50e24dcca9e");//read
+		}).then(function(rc) {
+			euc.rCha=rc;
+			//read
+			euc.rCha.on('characteristicvaluechanged', function(event) {
+				euc.alert=0;
+				if (event.target.value.buffer[0]==90) {
+					//print(event.target.value.buffer);
+					//batt
+					euc.dash.bat=event.target.value.getUint16(15, true);
+					if ((euc.dash.bat) >= euc.dash.batH) euc.dash.batC=0;
+					else  if ((euc.dash.bat) >= euc.dash.batM) euc.dash.batC=1;
+					else  if ((euc.dash.bat) >= euc.dash.batL) euc.dash.batC=2;
+					else  {
+						euc.dash.batC=3;
+						if (euc.dash.hapB) euc.alert++;
+					}
+					//speed
+					//euc.dash.spd=event.target.value.getUint16(17, true)/100;
+					//euc.dash.spdC = ( euc.dash.spd <= euc.dash.spd1 )? 0 : ( euc.dash.spd <= euc.dash.spd1+5 )? 1 : ( euc.dash.spd <= euc.dash.spd1+10 )? 2 : 3 ;	
+					//if ( euc.dash.hapS && euc.dash.spd >= euc.dash.spd1 ) 
+					//	euc.alert = 1 + ((euc.dash.spd-euc.dash.spd1) / euc.dash.ampS|0) ;
+				}else  if (event.target.value.buffer[1]){
+					//print(event.target.value.buffer);
+					euc.dash.trpT=(event.target.value.getUint32(1, true)/1000).toFixed(0);
+					if (!euc.dash.trpS) euc.dash.trpS=(event.target.value.getUint32(1, true)/1000).toFixed(3);
+					euc.dash.trpL=(event.target.value.getUint32(1, true)/1000).toFixed(3)-euc.dash.trpS;
+					euc.dash.time=(event.target.value.getUint16(7, true)/60)|0;
+					//temp
+					euc.dash.tmp=(event.target.value.getUint16(9, true)/10).toFixed(1);
+					euc.dash.tmpC = (euc.dash.tmp <= euc.dash.tmpH)? 0 : (euc.dash.tmp <= euc.dash.tmpH+5)? 2 : 3;	
+					if (euc.dash.tmpH <= euc.dash.tmp) {euc.alert++; euc.dash.spdC = 3;}   
+					//volt
+					euc.dash.volt=(event.target.value.getUint16(11, true)/100).toFixed(2);
+					//amp
+					euc.dash.amp=(event.target.value.getInt16(13, true)/100)|0;
+					ampL.unshift(euc.dash.amp);
+					if (14<ampL.length) ampL.pop();
+					euc.dash.ampC = ( euc.dash.ampH+10 <= euc.dash.amp || euc.dash.amp <= euc.dash.ampL - 5 )? 3 : ( euc.dash.ampH <= euc.dash.amp || euc.dash.amp <= euc.dash.ampL )? 2 : ( euc.dash.amp < 0 )? 1 : 0;
+					if ( euc.dash.ampH <= euc.dash.amp ){
+						euc.dash.spdC = ( euc.dash.ampC === 3 )? 3 : ( euc.dash.spdC === 3 )? 3 : 2;
+						if (euc.dash.hapA) euc.alert = ( euc.alert + 1 + ((euc.dash.amp - euc.dash.ampH) / euc.dash.ampS|0) );
+					}else if ( euc.dash.amp <= euc.dash.ampL )  {
+						euc.dash.spdC = (euc.dash.ampC === 3)? 3 : (euc.dash.spdC === 3)? 3 : 2;
+						if (euc.dash.hapA) euc.alert = (euc.alert + 1 + ((-(euc.dash.amp - euc.dash.ampL)) / euc.dash.ampS|0));  				
+					}
+					//speed
+					euc.dash.spd=event.target.value.getUint16(15, true)/100;
+					euc.dash.spdC = ( euc.dash.spd <= euc.dash.spd1 )? 0 : ( euc.dash.spd <= euc.dash.spd1+5 )? 1 : ( euc.dash.spd <= euc.dash.spd1+10 )? 2 : 3 ;	
+					if ( euc.dash.hapS && euc.dash.spd >= euc.dash.spd1 ) 
+						euc.alert = 1 + ((euc.dash.spd-euc.dash.spd1) / euc.dash.ampS|0) ;
+					//average
+					euc.dash.spdA=((event.target.value.getUint16(17, true))/100).toFixed(1);
+					//euc.dash.spdM=((event.target.value.getUint16(19, true))/100).toFixed(1);
+				}
+				//haptic
+				if (!euc.buzz && euc.alert) {  
+						euc.buzz=1;
+						if (20 <= euc.alert) euc.alert = 20;
+						var a=[];
+						while (5 <= euc.alert) {
+							a.push(200,500);
+							euc.alert = euc.alert - 5;
+						}
+						let i;
+						for (i = 0; i < euc.alert ; i++) {
+							a.push(200,150);
+						}
+						digitalPulse(D16,0,a);  
+						setTimeout(() => { euc.buzz = 0; }, 3000);
+				}
+				//screen on
+				if ((1<euc.dash.spdC||1<euc.dash.ampC)&&!w.gfx.isOn ){
+					face.go(set.dash[set.def.dash],0);
+				}
+			});
+			//on disconnect
+			global["\u00ff"].BLE_GATTS.device.on('gattserverdisconnected', function(reason) {
+				euc.off(reason);
+			});
+			euc.rCha.startNotifications();	
+			return  rc;
+		}).then(function(c) {
+			//connected 
+			if (set.def.cli) console.log("EUC: Connected"); 
+			euc.state="READY"; //connected
+			digitalPulse(D16,1,[90,40,150,40,90]);
+			euc.dash.lock=0;
+			//write function
+			euc.wri=function(cmd){
+			//print ("lala",cmd,euc.cmd(cmd));
+				if (euc.state==="OFF") {
+					if (euc.loop) {clearTimeout(euc.loop); euc.loop=0;}
+					setTimeout(()=>{global["\xFF"].BLE_GATTS.disconnect().catch(function(err)  {if (set.def.cli) console.log("EUC OUT disconnect failed:", err);});},200);
+				} else {
+					euc.wCha.writeValue(euc.cmd(cmd)).then(function() {
+						if (!euc.busy) { 
+							euc.loop=setTimeout(function(t,o){
+								euc.loop=0;
+								euc.wri("live");	
+							},50);
+						}
+					}).catch(function(err)  {
+						euc.off("writefail");	
+					});
+				}
+			};
+			euc.busy=0;
+			setTimeout(() => {euc.wri("live");}, 500);
+		//reconnect
+		}).catch(function(err)  {
+			euc.off(err);
+	});
+};
+
+euc.off=function(err){
+	//if (set.def.cli) console.log("EUC:", err);
+	//  global.error.push("EUC :"+err);
+	if (euc.tmp.loop) {clearInterval(euc.tmp.loop);euc.tmp.loop=0;}
+	if (euc.reconnect) {clearTimeout(euc.reconnect); euc.reconnect=0;}
+	if (euc.state!="OFF") {
+		if (set.def.cli) console.log("EUC: Restarting");
+		if ( err==="Connection Timeout"  )  {
+			if (set.def.cli) console.log("reason :timeout");
+			euc.state="LOST";
+			if (euc.dash.lock==1) digitalPulse(D16,1,250);
+			else digitalPulse(D16,1,[250,200,250,200,250]);
+			euc.reconnect=setTimeout(() => {
+				euc.reconnect=0;
+				euc.conn(euc.mac); 
+			}, 5000);
+		}else if ( err==="Disconnected"|| err==="Not connected")  {
+			if (set.def.cli) console.log("reason :",err);
+			euc.state="FAR";
+			// if (euc.dash.lock==1) digitalPulse(D16,1,100);
+			// else digitalPulse(D16,1,[100,150,100]);
+			euc.reconnect=setTimeout(() => {
+				euc.reconnect=0;
+				euc.conn(euc.mac); 
+			}, 500);
+		} else {
+			if (set.def.cli) console.log("reason :",err);
+			euc.state="RETRY";
+			euc.reconnect=setTimeout(() => {
+				euc.reconnect=0;
+				euc.conn(euc.mac); 
+			}, 1500);
+		}
+	} else {
+		if (set.def.cli) console.log("EUC: OUT");
+		global["\xFF"].bleHdl=[];
+			delete euc.off;
+			delete euc.conn;
+			delete euc.wri;
+			delete euc.tmp;
+			delete euc.cmd;
+			delete euc.dash.trpS;
+			delete euc.serv;
+			delete euc.wCha;
+			delete euc.rCha;
+			NRF.setTxPower(set.def.rfTX);	
+    }
+};
