5a071fb2756a4f2d7175bec4d02443967257fcf3
enaon <enaon@enaon>
2021-07-09 12:54:51 +0300

    Update comms.js

--- Loader/core/js/comms.js
+++ Loader/core/js/comms.js
@@ -240,10 +240,10 @@ const Comms = {
       });
     });
   },
-  setRead : (id) => {
+  setRead : (file,id) => {
     return new Promise((resolve,reject) => {
       let cmd = '\x03\x10';
-      cmd += "set.read("+file+","+id+")\n";
+      cmd += "set.read("+file+","+id+")\n"";
       Puck.write(cmd, (result) => {
         if (result===null) return reject("");
         resolve();
