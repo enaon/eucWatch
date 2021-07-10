083c584a22f9135367465a2da7e42810a11933d7
enaon <enaon@enaon>
2021-07-10 12:25:52 +0300

    Update comms.js

--- Loader/core/js/comms.js
+++ Loader/core/js/comms.js
@@ -20,13 +20,13 @@ const Comms = {
 		Comms.readSettings("setting","acctype").then(function(c) {
 			return localStorage.setItem("p8acc", c);
 			}).then(function(c) {
-			return Comms.readSettings("setting","touchtype");
+			Comms.readSettings("setting","touchtype");
 			}).then(function(c) {
-			return localStorage.setItem("p8touch", c);
+			localStorage.setItem("p8touch", c);
 			}).then(function(c) {
-			return Comms.readSettings("setting","name");
+			Comms.readSettings("setting","name");
 			}).then(function(c) {
-			return localStorage.setItem("p8name", c);
+			localStorage.setItem("p8name", c);
 			}).then(function(c) {	
 			setTimeout(resolve,100);
 			});
