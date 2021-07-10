4397ad92bee52d638ea361633debefb88231eec8
enaon <enaon@enaon>
2021-07-10 20:53:07 +0300

    Update comms.js

--- Loader/core/js/comms.js
+++ Loader/core/js/comms.js
@@ -123,20 +123,19 @@ const Comms = {
 		  //Comms.readSettings("setting","touchtype").then(function(c) {localStorage.setItem("p8touch", c);});
 		  //Comms.readSettings("setting","name").then(function(c) {localStorage.setItem("p8name", c);});
           resolve(appList);
-		 setTimeout(()=>{
-		 Comms.readSettings("setting","acctype").then(function(c) {
-			return localStorage.setItem("p8acc", c);
-			}).then(function(c) {
-			return Comms.readSettings("setting","touchtype");
-			}).then(function(c) {
-			return localStorage.setItem("p8touch", c);
-			}).then(function(c) {
-			return Comms.readSettings("setting","name");
-			}).then(function(c) {
-			 localStorage.setItem("p8name", c);
-		 
-		});
-		},1000);
+		  setTimeout(()=>{
+			 Comms.readSettings("setting","acctype").then(function(c) {
+				return localStorage.setItem("p8acc", c);
+				}).then(function(c) {
+				return Comms.readSettings("setting","touchtype");
+				}).then(function(c) {
+				return localStorage.setItem("p8touch", c);
+				}).then(function(c) {
+				return Comms.readSettings("setting","name");
+				}).then(function(c) {
+				 localStorage.setItem("p8name", c);
+			});
+		  },1000);
         }, true /* callback on newline */);
       });
     });
