299a71c302707232ce3864ef613bb88e19df31fd
enaon <enaon@enaon>
2021-07-11 03:34:31 +0300

    Update comms.js

--- Loader/core/js/comms.js
+++ Loader/core/js/comms.js
@@ -8,18 +8,7 @@ const Comms = {
   reset : (opt) => new Promise((resolve,reject) => {
     let tries = 8;
     console.log("<COMMS> reset");
-	Comms.readSettings("setting","acctype").then(function(c) {
-			localStorage.setItem("p8acc", c);
-		}).then(function() {
-			return Comms.readSettings("setting","touchtype");
-		}).then(function(c) {
-			localStorage.setItem("p8touch", c);
-		}).then(function() {
-			return Comms.readSettings("setting","name");
-		}).then(function(c) {
-			localStorage.setItem("p8name", c);
-		}).then(function() {
-			Puck.write(`\x03\x10reset(${opt=="wipe"?"1":""});\n`,function rstHandler(result) {
+	Puck.write(`\x03\x10reset(${opt=="wipe"?"1":""});\n`,function rstHandler(result) {
 			console.log("<COMMS> reset: got "+JSON.stringify(result));
 			if (result===null) return reject("Connection failed");
 			if (result=="" && (tries-- > 0)) {
@@ -28,8 +17,19 @@ const Comms = {
 			} else {
 				console.log(`<COMMS> reset: complete.`);
 				setTimeout(resolve,250);
+				 Comms.readSettings("setting","acctype").then(function(c) {
+				return localStorage.setItem("p8acc", c);
+				}).then(function(c) {
+				return Comms.readSettings("setting","touchtype");
+				}).then(function(c) {
+				return localStorage.setItem("p8touch", c);
+				}).then(function(c) {
+				return Comms.readSettings("setting","name");
+				}).then(function(c) {
+				 localStorage.setItem("p8name", c);
+				});
 			}
-		});
+		
 	});
   }),
   uploadApp : (app,skipReset) => { // expects an apps.json structure (i.e. with `storage`)
