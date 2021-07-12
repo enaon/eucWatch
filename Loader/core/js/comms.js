95fc2ef7632d0271b546f91564c2577207d1f33d
enaon <enaon@enaon>
2021-07-13 00:49:04 +0300

    Update comms.js

--- Loader/core/js/comms.js
+++ Loader/core/js/comms.js
@@ -106,7 +106,7 @@ const Comms = {
   getInstalledApps : () => {
     Progress.show({title:`Getting app list...`,sticky:true});
     return new Promise((resolve,reject) => {
-      Puck.write("\x04",(result) => {
+      Puck.write("\x03",(result) => {
         if (result===null) {
           Progress.hide({sticky:true});
           return reject("");
