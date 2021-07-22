d52dbf212e93eec7c577806593d97fea2544b52d
enaon <enaon@enaon>
2021-07-22 12:19:56 +0300

    Update comms.js

--- Loader/core/js/comms.js
+++ Loader/core/js/comms.js
@@ -17,6 +17,8 @@ const Comms = {
 			} else {
 				console.log(`<COMMS> reset: complete.`);
 				setTimeout(resolve,250);
+				setTimeout(()=>{
+
 				Comms.readSettings("setting","acctype").then(function(c) {
 					return localStorage.setItem("p8acc", c);
 				}).then(function() {
@@ -28,6 +30,8 @@ const Comms = {
 				}).then(function(c) {
 					localStorage.setItem("p8name", c);
 				});
+				},1000);
+
 			}
 		
 	});
