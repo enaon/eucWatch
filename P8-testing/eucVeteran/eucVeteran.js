673c7e3bcdd655a607e055d1e98238d17d5e2fbe
enaon <enaon@enaon>
2021-07-30 02:28:14 +0300

    Update eucVeteran.js

--- P8-testing/eucVeteran/eucVeteran.js
+++ P8-testing/eucVeteran/eucVeteran.js
@@ -40,7 +40,6 @@ euc.conn=function(mac){
 				if (20<batL.length) batL.pop();
 				euc.dash.spd=Math.round((this.event[6] << 8 | this.event[7]) / 10);
 				if (euc.dash.spdM <euc.dash.spd) euc.dash.spd = euc.dash.spdM;
-				
 				euc.dash.trpL=(this.event[10] << 24 | this.event[11] << 16 | this.event[8] << 8  | this.event[9])/1000;
 				euc.dash.trpT=(this.event[14] << 24 | this.event[15] << 16 | this.event[12] << 8  | this.event[13])/1000;
 				if (!euc.log.trpS) euc.log.trpS=euc.dash.trpT;
