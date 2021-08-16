ae8c5556bf28dcce0756afef7e3b4e67e0ac0f17
enaon <enaon@enaon>
2021-08-16 19:13:41 +0300

    up

--- P8-testing/handler/handler.js
+++ P8-testing/handler/handler.js
@@ -387,13 +387,10 @@ if (set.def.touchtype=="816"){ //816
 	setWatch(function(s){
 		i2c.writeTo(0x15,0);
 		var tp=i2c.readFrom(0x15,7);
-		print("touch816 :",tp);
+		//print("touch816 :",tp);
 		if (face.pageCurr>=0) {
-			if (tp[1]== 0 && tp[3]==64) tp[1]=5;
-			if (tp[1]== 12) {
-				tp[6]=tp[6]+25;
-				print("touch816 event 12 new y:",tp[6]);
-				}
+			if (tp[1]== 0 && tp[3]==64) {tp[1]=5; set.p22=1;}
+			if (set.p22 && tp[1]== 12 ) tp[6]=tp[6]+25;
 			touchHandler[face.pageCurr](tp[1],tp[4],tp[6]);}
 		else if (tp[1]==1) {
 			face.go(face.appCurr,0);
