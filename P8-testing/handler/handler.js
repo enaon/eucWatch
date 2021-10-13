825672d5c1c5cc2fccdfce3547f8f13ae342b0e5
enaon <enaon@enaon>
2021-10-14 01:37:25 +0300

    Update handler.js

--- P8-testing/handler/handler.js
+++ P8-testing/handler/handler.js
@@ -471,6 +471,7 @@ if (set.def.touchtype=="816"){ //816
 			}
 			if ( this.do && getTime() - this.time > 1 ) { 
 				this.do = 0 ;
+				return setTimeout(function() {touchHandler[face.pageCurr](12,tfk.x,tfk.y);},0);
 				touchHandler[face.pageCurr](12,this.x,this.y);
 				return;
 			}else if ( this.do && !tp[1] ) {
@@ -482,22 +483,18 @@ if (set.def.touchtype=="816"){ //816
 				if ( a != 0 && this.aLast != a ) {
                     this.aLast=a;
 					this.do=0;
-					touchHandler[face.pageCurr](a,this.x,this.y);
-					return;
+					return setTimeout(function() {touchHandler[face.pageCurr](a,tfk.x,tfk.y);},0);
 				}
 			}else if ( this.do ){
 				if ( tp[1] == 5 || tp[1] ==12 ){
 					this.do=0;
-					//tfk.emit("touch",
-                    touchHandler[face.pageCurr](tp[1],this.x,this.y);
-                    return;
+					return setTimeout(function() { touchHandler[face.pageCurr](tp[1],tfk.x,tfk.y);},0);
 				}
 			}
 		}else if ( (tp[3] == 255 || tp[3] == 0)  && !this.st ) {
 			if (this.do===1){
-              this.do=0;
-              touchHandler[face.pageCurr](5,this.x,this.y);
-			  return;
+				this.do=0;
+				return setTimeout(function() {touchHandler[face.pageCurr](5,tfk.x,tfk.y);},0);
             }
             this.aLast=0;
 			this.st = 1;
