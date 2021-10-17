71487bd180bc3cb723bf614884875e12812c57bb
enaon <enaon@enaon>
2021-10-17 04:11:53 +0300

    faster wake on turn

--- P8-testing/handler/handler.js
+++ P8-testing/handler/handler.js
@@ -629,7 +629,7 @@ if (set.def.acctype==="BMA421"){
 						}
 						this.up=0;
 					} else this.up=1;
-				},50);
+				},100);
 				return true;
 			}else if (!this.tid) {
 				i2c.writeTo(0x18,0x32,20); //int1_ths-threshold = 250 milli g's
