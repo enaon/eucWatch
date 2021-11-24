8a393c5b31bd5a4f118c73ad85fe67e1a706ad15
enaon <enaon@enaon>
2021-11-24 16:29:09 +0200

    Update handler.js

--- Magic-testing/handler/handler.js
+++ Magic-testing/handler/handler.js
@@ -101,7 +101,8 @@ var set={
 		buzz:1
 		};
 		set.updateSettings();
-	},
+	},	
+	accR:function(){if(!this.def.dash.accE) { if (this.def.acc)acc.on(); else acc.off();}},
 	accR:function(){if(!this.def.dash.accE) { if (this.def.acc)acc.on(); else acc.off();}},
 	hidM:undefined, //not user settable.
 	clin:0,//not settable
@@ -628,7 +629,7 @@ function col(no){
 		"ram";
 		switch (no) {
 			case "black":return 0;case "white":return 4095;case "lblue":return 1535;case "blue":return 143;case "dblue":return 1375;case "blue1":return 1708;
-			case "raf":return 1453;case "raf1":return 1708;case "raf2":return 1963;case "raf3":return 2220;case "raf4":return 2474;case "raf5":return 3005;
+			case "raf":return 1629;case "raf1":return 2474;case "raf2":return 1963;case "raf3":return 2220;case "raf4":return 2474;case "raf5":return 3005;
 			case "gray":return 2730;case "lgray":return 3549;case "dgray":return 1365;case "dgray1":return 1351;case "lgreen":return 1525;case "red":return 3840;
 			case "dred":return 3925;case "dred1":return 3888;case "purple":return 3935;case "lyellow":return 4085;case "dyellow":return 4064;case "yellow":return 4080;
 			case "olive":return 170;
