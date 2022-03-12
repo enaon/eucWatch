f5795602c57d6b22d1f8bb15439512b013d40132
enaon <enaon@enaon>
2022-03-12 07:56:14 +0200

    fix sc7a20 right hand usage

--- P8-testing/handler_pt/handler.js
+++ P8-testing/handler_pt/handler.js
@@ -709,16 +709,19 @@ if (set.def.acctype==="BMA421"){
 				this.tid= setInterval(()=>{	
 					"ram";
 					let cor=acc.read();
-					if (-1000<=cor.ax && cor.ax<=500 && cor.ay<=500 && cor.az<=-300 ) {
-						if (!w.gfx.isOn&&this.up){  
-								face.go(set.dash[set.def.dash.face],0);
-						}else {
-							let tout=set.def.off[face.appCurr];
-							if ( !tout || ( tout &&  tout <= 60000)) 
-								face.off(1500);
-						}
+					if (-1100<=cor.ax && cor.ax<=0 &&  -700<=cor.ay &&cor.ay<=1000 && cor.az<=-500 ) {
+						if (!w.gfx.isOn&&this.up)
+							face.go(set.dash[set.def.dash.face],0);
+						else if (w.gfx.isOn)  face.off(0);
 						this.up=0;
-					} else this.up=1;
+						changeInterval(acc.tid,2000)
+					} else if (!this.up) {
+						this.up=1;
+						let tout=set.def.off[face.appCurr];
+						if (w.gfx.isOn ) if ( !tout || ( tout &&  tout <= 60000)) 
+							face.off(1000);
+						changeInterval(acc.tid,100)
+					}
 				},100);
 				return true;
 			}else if (!this.tid) {
