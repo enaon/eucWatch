0cee872e588ba60b7610e19c4092f8b0a445e2ba
enaon <enaon@drinet.gr>
2022-08-10 03:50:18 +0300

    Update p8loader.js

--- P8/p8loader.js
+++ P8/p8loader.js
@@ -2,7 +2,7 @@
 /* BLE debug info */
 //Puck.debug = 3;
 //Const.SINGLE_APP_ONLY = false;
-APP_SOURCECODE_DEV ="P8-testing";
+APP_SOURCECODE_DEV ="P8";
 Const.APP_SOURCECODE_URL = `https://github.com/enaon/eucWatch/tree/main/`;
 
 
