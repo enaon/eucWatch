976b8057ddc03507355402c415f7f4adef5d3d5a
Igor Vasilkov <igor@kvazimoda.pro>
2023-09-24 05:42:45 +0300

    Improvement of the TPMS module
    * Increased sensors scanning time
    * Fixed several bugs

--- v2/handler/handler_notify.js
+++ v2/handler/handler_notify.js
@@ -10,7 +10,7 @@ function handleInfoEvent(event,discrete) {
 	//"ram";
 	notify.nInfo++;
 	notify.New++;
-	
+
 	let d=(Date()).toString().split(' ');
     let ti=(""+d[4]+" "+d[0]+" "+d[2]);
 	notify.info.unshift("{\"src\":\""+event.src+"\",\"title\":\""+event.title+"\",\"body\":\""+event.body+"\",\"time\":\""+ti+"\"}");
@@ -19,7 +19,7 @@ function handleInfoEvent(event,discrete) {
 		face.off(8000);
 		buzzer.nav([80,50,80]);
 		if (face[0].bar){
-			UI.btn.ntfy(1,4,0,"_bar",6,event.title,event.body,0,15);w.gfx.flip();
+			if (typeof UI !== "undefined") { UI.btn.ntfy(1,4,0,"_bar",6,event.title,event.body,0,15);w.gfx.flip(); }
 		}else if (!discrete){
 			if (face.appCurr!="clock"||face.pageCurr!=0) {
 				face.go("clock",0);
